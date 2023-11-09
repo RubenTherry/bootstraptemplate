d9dd7b343c0327eb81f7302a6ce28b66bfb7a9d2
RubenTherry <therry.ruben@hotmail.com>
2023-11-09 13:20:25 +0100

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+// Import our custom CSS
+import '../scss/styles.scss';
+import 'bootstrap-icons/font/bootstrap-icons.scss';
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
